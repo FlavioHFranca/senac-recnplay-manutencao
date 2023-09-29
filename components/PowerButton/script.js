58fd301f2b351b0f69896f8cf5b82d7e0e8ad7a8
Marcos Aurélio <aurliomarcos@gmail.com>
2023-09-28 23:20:22 -0300

    change(component):refactor power button:hammer:

--- components/PowerButton/script.js
+++ components/PowerButton/script.js
@@ -1,42 +1,35 @@
 import CreateIcon from "../../constants/CreateIcon.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  try {
-    const powerButton = document.createElement("button");
-  
-    powerButton.id = "power_button";
-    powerButton.classList.add("power_button");
-    powerButton.classList.add("off");
-    powerButton.appendChild(CreateIcon("fa-power-off"));
-    powerButton.onclick = () => onOff();
-  
-    const powerButtonRoot = document.getElementById("power_button_root");
-    powerButtonRoot.appendChild(powerButton);
+document.addEventListener("DOMContentLoaded", () => PowerButton());
 
-  } catch (e) {
+const PowerButton = () => {
+  try {
+    const root = document.getElementById("power_button_root");
+    root.appendChild(createComponent());
+  } catch(e) {
     console.log(`Error: ${e.message}`);
   };
-});
+};
 
-function onOff() {
-  try {
-    const powerButton = document.getElementById("power_button");
+const createComponent = () => {
+  const button = document.createElement("button");
+  button.id = "pb_btn";
+  button.classList.add("pb_btn");
+  button.classList.add("off");
+  button.onclick = () => onOff(button.id);
 
-    if (powerButton.classList.contains("off")) {
-      powerButton.classList.remove("off");
-      powerButton.classList.add("on");
+  button.appendChild(CreateIcon("fa-power-off"));
 
-      console.log("Você ligou o computador.");
+  return button;
+};
 
-    } else {
-      powerButton.classList.remove("on");
-      powerButton.classList.add("off");
-
-      console.log("Você desligou o computador.");
-
-    };
-
-  } catch (e) {
-    console.log(`Erro: ${e.message}}`);
-  };
+const onOff = (id_element) => {
+  const element = document.getElementById(id_element);
+  if (element.classList.contains("on")) {
+    element.classList.remove("on");
+    element.classList.add("off");
+  } else {
+    element.classList.remove("off");
+    element.classList.add("on");
+  }
 };
