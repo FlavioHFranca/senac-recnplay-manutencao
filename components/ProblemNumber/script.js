b0acdcba86a76aae553c578aa78d1bdd0ab64107
Marcos Aurélio <aurliomarcos@gmail.com>
2023-10-07 16:49:25 -0300

    change(game):defect name:hammer:

--- components/ProblemNumber/script.js
+++ components/ProblemNumber/script.js
@@ -1,26 +1,33 @@
 import CreateIcon from "../../constants/CreateIcon.js";
 
-const ProblemNumber = (problemNumber) => {
+const ProblemNumber = async () => {
   try {
     const problemNumberRoot = document.getElementById("problem_number_root");
-    problemNumberRoot.appendChild(createComponent(problemNumber));
+    const componentSection = await createComponent();
+    problemNumberRoot.appendChild(componentSection);
   } catch (e) {
     console.log(`Error: ${e.message}`);
   }
 };
 
-const createComponent = (parameter) => {
+const createComponent = async () => {
   const section = document.createElement("section");
   section.className = "problem_number";
 
   const icon = CreateIcon("fa-screwdriver-wrench");
   const span = document.createElement("span");
-  span.textContent = parameter ? parameter : "Defeito 1";
+
+  const currentProblem = await getCurrentProblem();
+  span.textContent = `Defeito ${currentProblem.id}`;
 
   section.append(icon);
   section.append(span);
 
   return section;
+};
+
+const getCurrentProblem = async () => {
+  return JSON.parse(localStorage.getItem("current_problem"));
 };
 
 ProblemNumber();
